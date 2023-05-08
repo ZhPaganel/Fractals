cf11bd4e60c861ba72c1ce372d8202101d8d56b2
ZhPaganel <95427867+ZhPaganel@users.noreply.github.com>
2023-05-08 12:57:22 +0300

    Update fractals.py

--- fractals.py
+++ fractals.py
@@ -6,51 +6,33 @@ rt(270)
 
 seth(90)
 
-angle = 20
+angle = 30
+pu()
+setpos(0, -200)
+pd()
 
 def tree(level, size):
     if level > 0:
-        stamp()
 
-        width(level * 0.5)
+        colormode(255)
+    
+        pencolor(0, 255//level, 0)
 
         fd(size)
 
         lt(angle)
         tree(level - 1, size * 0.8)
 
+        pencolor(0, 255//level, 0)
+
         rt(angle*2)
         tree(level - 1, size * 0.8)
 
-        lt(angle)
+        pencolor(0, 255//level, 0)
 
+        lt(angle)
         bk(size)     
 
-        shapesize(2) 
-   
-
-def draw(level):
-
-    colormode(255)
-
-    pu()
-
-    home()
-
-    hideturtle()
-    seth(90)
-
-    bk(200)
-
-    pd()
-
-    pencolor('brown')
-    shape("circle")
-    resizemode("user")
-    fillcolor('green')
-
-    tree(level, 120)
-
-draw(5)
+tree(14, 120)
 
 input()
